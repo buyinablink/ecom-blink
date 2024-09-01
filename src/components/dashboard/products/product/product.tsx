9844fd724ead658df996d0ae863efbdc5fff7858
ved08 <vedvardhan.g@gmail.com>
2024-09-01 22:30:36 +0530

    changed add products ui/ux

--- src/components/dashboard/products/product/product.tsx
+++ src/components/dashboard/products/product/product.tsx
@@ -7,8 +7,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import PopoverButton from "./popoverbtn";
+import { Plus } from "lucide-react";
+import Modal from "@/components/modal";
+import { Button } from "@/components/ui/button";
+import ProductForm from "./product-form";
 
 export default function Product(props: {
   name: string;
@@ -27,15 +31,17 @@ export default function Product(props: {
   };
 
   return (
-    <Card className="w-[250px] h-fit">
-      <CardHeader>
+    <Card className="w-[250px] h-[530px]">
+      <CardHeader className="h-[120px]">
         <CardTitle>{productData.title}</CardTitle>
         <CardDescription>{productData.name}</CardDescription>
       </CardHeader>
       <CardContent>
-        <img src={productData.imageUrl} alt={productData.name} />
-        <CardDescription>{productData.label}</CardDescription>
-        <CardDescription>{productData.description}</CardDescription>
+        <div className="h-[300px]">
+          <img src={productData.imageUrl} alt={productData.name} width={250}/>
+          <CardDescription>{productData.label}</CardDescription>
+          <CardDescription>{productData.description}</CardDescription>
+        </div>
         <p>Price: {productData.price}</p>
         <p>In Stock: {productData.stock}</p>
         <PopoverButton
@@ -44,5 +50,25 @@ export default function Product(props: {
         />
       </CardContent>
     </Card>
+  );
+}
+export function AddNewProduct() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+  const addNewProduct = () => {
+    console.log("Add new product");
+    openModal();
+  };
+  return (
+    <Fragment>
+      <Card onClick={openModal} className="flex justify-center content-center w-[250px] h-[100%] hover:bg-[#f8fafc] cursor-pointer">
+            <Plus size={48} className="self-center"/>
+      </Card>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <h1>Add New Product</h1>
+        <ProductForm closeModal={closeModal}/>
+      </Modal>
+    </Fragment>
   );
 }
--- src/components/dashboard/products/products.tsx
+++ src/components/dashboard/products/products.tsx
@@ -1,16 +1,16 @@
 "use client";
 import { Fragment, useEffect, useState } from "react";
-import Product from "./product/product";
+import Product, { AddNewProduct } from "./product/product";
 import Navbar from "../../navbar/Navbar";
 import Loading from "@/components/Loading";
 import { useGetSellerProducts } from "@/hooks/useGetUser";
-import AddProduct from "./AddProduct";
 
 export default function Products({ address }: { address: string }) {
   const [products, setProducts] = useState<any>(null);
   const { data, isLoading } = useGetSellerProducts(address);
 
   useEffect(() => {
+    console.log(address)
     if (data && !data.err && data.data) {
       setProducts(data.data);
     }
@@ -23,18 +23,18 @@ export default function Products({ address }: { address: string }) {
     <Fragment>
       <Navbar />
       <>
-        <div className="flex justify-between p-3">
+        <div className="grid grid-cols-3 justify-center m-5">
+          <div> </div>
           <h1 className="text-4xl text-center subpixel-antialiased font-bold">
             Your Products
           </h1>
-          <div className="justify-self-end">
-            <AddProduct address={address} />
-          </div>
+          <div className="justify-self-end"></div>
         </div>
-        <div className="flex justify-start border p-3 ">
+        <div className="flex justify-center m-4">
           <ProductsDataRender products={products} />
         </div>
       </>
+      )
     </Fragment>
   );
 }
@@ -43,7 +43,7 @@ function ProductsDataRender({ products }: any) {
   return (
     <>
       <div className="grid gap-4 justify-center md:justify-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {products &&
+        {products ?
           products.map((product: any) => (
             <Product
               key={product.id}
@@ -56,7 +56,8 @@ function ProductsDataRender({ products }: any) {
               id={product.id}
               label={product.label}
             />
-          ))}
+          )) : <div className="text-2xl text-center">No Products Found</div>}
+          <AddNewProduct />
       </div>
     </>
   );
