711e8f275d8b9759e1bb7988559296b00c798a00
ved08 <vedvardhan.g@gmail.com>
2024-09-02 00:27:47 +0530

    tweaked navbar

--- src/components/navbar/UserSetting.tsx
+++ src/components/navbar/UserSetting.tsx
@@ -12,55 +12,82 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { getTheUser } from "@/lib/action";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 export function UserSetting() {
   const router = useRouter();
+  const {publicKey, connected} = useWallet()
+  const [userData, setUserData] = useState<any>(null)
+  useEffect(() => {
+    (async() => {
+      if(publicKey) {
+        const userData = await getTheUser(publicKey.toString())
+        if(userData.err) return;
+        setUserData(userData.data)
+        console.log(userData.data)
+      }
+    })()
+  }, [])
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-          <Avatar className="h-8 w-8">
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="mt-2 w-56" align="end" forceMount>
-        <DropdownMenuGroup>
-          <DropdownMenuItem
-            className="cursor-pointer"
-            onClick={() => {
-              router.push("/dashboard");
-            }}
-          >
-            DashBoard
-          </DropdownMenuItem>
-
-          <DropdownMenuItem
-            onClick={() => {
-              router.push("/dashboard/blink");
-            }}
-          >
-            Your Blink
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            className="cursor-pointer"
-            onClick={() => {
-              router.push("/dashboard/orders");
-            }}
-          >
-            Orders
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            className="cursor-pointer"
-            onClick={() => {
-              router.push("/dashboard/products");
-            }}
-          >
-            Products
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    connected && publicKey && userData &&
+        <>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+              <Avatar className="h-11 w-11 border">
+                <AvatarImage src={`https://robohash.org/${userData.walletAddress}?set=set4`} alt="@shadcn" />
+                <AvatarFallback>SC</AvatarFallback>
+              </Avatar>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="mt-2 w-56" align="end" forceMount>
+            <DropdownMenuLabel className="font-normal">
+              <div className="flex flex-col space-y-1">
+                <p className="text-sm font-medium leading-none">Your Account</p>
+                <p className="text-xs leading-none text-muted-foreground">
+                  {userData.username}
+                </p>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem
+                className="cursor-pointer"
+                onClick={() => {
+                  router.push("/dashboard");
+                }}
+              >
+                DashBoard
+              </DropdownMenuItem>
+    
+              <DropdownMenuItem
+                onClick={() => {
+                  router.push("/dashboard/blink");
+                }}
+              >
+                Your Blink
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="cursor-pointer"
+                onClick={() => {
+                  router.push("/dashboard/orders");
+                }}
+              >
+                Orders
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="cursor-pointer"
+                onClick={() => {
+                  router.push("/dashboard/products");
+                }}
+              >
+                Products
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </>
   );
 }
