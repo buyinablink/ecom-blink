85e0722f6f00880da951fe10267665414e7843bd
ved08 <vedvardhan.g@gmail.com>
2024-10-08 18:54:24 +0530

    small change to navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   const router = useRouter();
   return (
     // <header className="p-3 flex items-center justify-between border bg-[#2C303B]">
-    <header className="flex items-center justify-between p-3 sticky top-0 bg-[#2C303B]">
+    <header className="flex items-center justify-between p-3 bg-[#2C303B]">
       <Image width={0} height={0} src={logo} alt="Buy in a Blink" className="w-auto h-8 lg:h-10"/>
       <div className="flex p-2 gap-4 items-center">
         <WalletMultiButtonFix />
